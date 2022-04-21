f4e27c596e7fc941802fbc69b6f8d86231c2d11a
Filipe Moura <filipeasm18@gmail.com>
2022-04-21 15:42:03 -0300

    first commit

---
+++ src/main/java/springseller/filipey/SpringSellerApplication.java
@@ -0,0 +1,20 @@
+package springseller.filipey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringSellerApplication {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSellerApplication.class, args);
+    }
+}
