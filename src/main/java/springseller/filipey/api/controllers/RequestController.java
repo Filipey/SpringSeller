ad70b8a64da2586a0c6e23ef7aea28df0dc428ed
Filipe Moura <filipeasm18@gmail.com>
2022-04-25 22:11:26 -0300

    created post route for request

--- src/main/java/springseller/filipey/api/controllers/RequestController.java
+++ src/main/java/springseller/filipey/api/controllers/RequestController.java
@@ -1,8 +1,10 @@
 package springseller.filipey.api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import springseller.filipey.api.dto.RequestDTO;
 import springseller.filipey.services.RequestService;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/api/v1/requests")
@@ -12,5 +14,11 @@ public class RequestController {
 
     public RequestController(RequestService requestService) {
         this.requestService = requestService;
+    }
+
+    @PostMapping
+    @ResponseStatus(CREATED)
+    public Long save( @RequestBody RequestDTO dto ) {
+        return requestService.save(dto).getId();
     }
 }
