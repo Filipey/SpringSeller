be756bc39fd663715c6e94a4d480294e42d1f4b6
Filipe Moura <filipeasm18@gmail.com>
2022-04-26 10:21:38 -0300

    included handle api exceptions

---
+++ src/main/java/springseller/filipey/api/ApiErrors.java
@@ -0,0 +1,18 @@
+package springseller.filipey.api;
+
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErrors {
+
+    @Getter
+    private List<String> errors;
+
+    public ApiErrors(String errorMessage) {
+        this.errors = Arrays.asList(errorMessage);
+    }
+
+
+}
