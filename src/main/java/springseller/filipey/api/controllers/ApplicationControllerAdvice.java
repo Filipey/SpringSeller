39568170ab21e66954c315a35a46bd8bbd23915b
Filipe Moura <filipeasm18@gmail.com>
2022-04-29 15:26:30 -0300

    handle validation exceptions

--- src/main/java/springseller/filipey/api/controllers/ApplicationControllerAdvice.java
+++ src/main/java/springseller/filipey/api/controllers/ApplicationControllerAdvice.java
@@ -1,6 +1,7 @@
 package springseller.filipey.api.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,6 +10,9 @@ import springseller.filipey.api.exception.ClientNotFoundException;
 import springseller.filipey.api.exception.ObjectNotFoundException;
 import springseller.filipey.api.exception.ProductNotFoundException;
 import springseller.filipey.api.exception.RequestNotFoundException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -25,15 +29,26 @@ public class ApplicationControllerAdvice {
         return new ApiErrors(exception.getMessage());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(ClientNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiErrors handleClientNotFoundException(ClientNotFoundException exception) {
         return new ApiErrors(exception.getMessage());
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(ProductNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiErrors handleProductNotFoundException(ProductNotFoundException exception) {
         return new ApiErrors(exception.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException exception) {
+        List<String> errors = exception.getBindingResult().getAllErrors()
+                .stream()
+                .map( e -> e.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new ApiErrors(errors);
     }
 }
