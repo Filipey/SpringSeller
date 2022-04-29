71627efdd7ea7f6fe0837b53c682e1e4c8463641
Filipe Moura <filipeasm18@gmail.com>
2022-04-29 16:19:10 -0300

    validated requests list

--- src/main/java/springseller/filipey/api/dto/RequestDTO.java
+++ src/main/java/springseller/filipey/api/dto/RequestDTO.java
@@ -3,6 +3,7 @@ package springseller.filipey.api.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import springseller.filipey.api.validation.NotEmptyList;
 
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
@@ -19,6 +20,6 @@ public class RequestDTO {
     @NotNull(message = "Amount field is required")
     private BigDecimal amount;
 
-
+    @NotEmptyList(message = "Request can not be done without products")
     private List<ProductRequestDTO> requests;
 }
