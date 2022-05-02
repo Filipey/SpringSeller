d502c0d8e87744ea1772603be9611bc3551135b3
Filipe Moura <filipeasm18@gmail.com>
2022-05-02 20:04:49 -0300

    add UserRepository

---
+++ src/main/java/springseller/filipey/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package springseller.filipey.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import springseller.filipey.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLogin(String login);
+}
