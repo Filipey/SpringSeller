b1bd0e9b72ca6afe58cd1bba342ae7c29481979a
Filipe Moura <filipeasm18@gmail.com>
2022-05-18 21:29:24 -0300

    configured Swagger

---
+++ src/main/java/springseller/webservice/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package springseller.webservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("springseller.webservice.api.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Seller API")
+                .description("Api para simulação de sistema de vendas")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact() {
+        return new Contact("Filipe Moura",
+                "http://github.com/Filipey",
+                "filipeasm18@gmail.com");
+    }
+}
